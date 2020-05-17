fe07d98f98018162a48ec4b8879c820ad40eb953
tevin davis <aod.td1105@gmail.com>
2020-05-17 19:39:45 -0400

    Added helper functions to users/user-model file

---
+++ users/user-model.js
@@ -0,0 +1,22 @@
+const bcrpyt = require('bcryptjs')
+const db = require('../database/dbConfig')
+
+function find() {
+    return db("users").select("id", "username")
+}
+
+function findById(id) {
+    return db("users").where("id", id).select("id", "username").first()
+}
+
+function findBy(user) {
+    return db("users").where(user).select('id', 'username', 'password')
+}
+
+function add(user) {
+    user.password = await bcrpyt.hash(user.password, 14)
+
+    const [id] = await db("users").insert(user)
+
+    return findById(id)
+}
